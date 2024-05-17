b1ae7a87126ac75edf268543655014e57760c3fa
dkpark10 <dkpark10@naver.com>
2024-05-17 10:23:10 +0900

    test: 스토어 카운터 테스트 추가

--- src/test/store.test.tsx
+++ src/test/store.test.tsx
@@ -1,7 +1,36 @@
+import React from 'react';
 import { expect, test } from 'vitest';
 import { render, fireEvent } from '@testing-library/react';
-import { useState } from 'react';
+import { useCounterStore, store } from '../store';
 
-test('스토어 테스트', () => {
+test('카운터', () => {
+  function Counter() {
+    const count = useCounterStore(
+      store,
+      (state) => state.count,
+    );
   
+    return (
+      <React.Fragment>
+        <button onClick={() => {
+          store.setState((prev) => ({ count: prev.count + 1 }));
+        }}>
+          inc
+        </button>
+        <button onClick={() => {
+          store.setState((prev) => ({ count: prev.count - 1 }));
+        }}>
+          dec
+        </button>
+        <h1>value: {count}</h1>
+      </React.Fragment>
+    )
+  }
+  
+  const { getByText, getByRole } = render(<Counter />);
+  fireEvent.click(getByText('inc'));
+  expect(getByRole('heading', { level: 1 }).textContent).toBe('value: 1');
+
+  fireEvent.click(getByText('dec'));
+  expect(getByRole('heading', { level: 1 }).textContent).toBe('value: 0');
 });
