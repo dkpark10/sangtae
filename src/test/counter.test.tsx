d863452fae60f8ee1ebb3605a37c0d133601a119
dkpark10 <dkpark10@naver.com>
2024-05-17 11:24:54 +0900

    test: 리렌더링 테스트

--- src/test/counter.test.tsx
+++ src/test/counter.test.tsx
@@ -1,34 +1,74 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { render, fireEvent } from '@testing-library/react';
 import { useState } from 'react';
 
-test('카운터', () => {
-  function Counter() {
-    const [c, sc] = useState(0);
-    return (
-      <div>
-        <button
-          onClick={() => {
-            sc(c + 1);
-          }}
-        >
-          inc
-        </button>
-        <button
-          onClick={() => {
-            sc(c - 1);
-          }}
-        >
-          dec
-        </button>
-        <h1>value: {c}</h1>
-      </div>
-    );
-  }
-  const { getByText, getByRole } = render(<Counter />);
-  fireEvent.click(getByText('inc'));
-  expect(getByRole('heading', { level: 1 }).textContent).toBe('value: 1');
+describe('useState', () => {
+  test('카운터', () => {
+    function Counter() {
+      const [c, sc] = useState(0);
+      return (
+        <div>
+          <button
+            onClick={() => {
+              sc(c + 1);
+            }}
+          >
+            inc
+          </button>
+          <button
+            onClick={() => {
+              sc(c - 1);
+            }}
+          >
+            dec
+          </button>
+          <h1>value: {c}</h1>
+        </div>
+      );
+    }
+    const { getByText, getByRole } = render(<Counter />);
+    fireEvent.click(getByText('inc'));
+    expect(getByRole('heading', { level: 1 }).textContent).toBe('value: 1');
 
-  fireEvent.click(getByText('dec'));
-  expect(getByRole('heading', { level: 1 }).textContent).toBe('value: 0');
+    fireEvent.click(getByText('dec'));
+    expect(getByRole('heading', { level: 1 }).textContent).toBe('value: 0');
+  });
+
+  test('리렌더링', () => {
+    let renderCount = 0;
+
+    function Counter() {
+      const [c, sc] = useState(0);
+      renderCount += 1;
+      return (
+        <div>
+          <button
+            onClick={() => {
+              sc(c + 1);
+            }}
+          >
+            inc
+          </button>
+          <button
+            onClick={() => {
+              sc(c);
+            }}
+          >
+            dec
+          </button>
+          <h1>value: {c}</h1>
+        </div>
+      );
+    }
+    const { getByText } = render(<Counter />);
+
+    expect(renderCount).toBe(1);
+    fireEvent.click(getByText('dec'));
+    fireEvent.click(getByText('dec'));
+    fireEvent.click(getByText('dec'));
+    expect(renderCount).toBe(1);
+
+    fireEvent.click(getByText('inc'));
+    expect(renderCount).toBe(2);
+  });
 });
--- src/test/store.test.tsx
+++ src/test/store.test.tsx
@@ -1,36 +1,77 @@
 import React from 'react';
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { render, fireEvent } from '@testing-library/react';
 import { useCounterStore, store } from '../store';
 
-test('카운터', () => {
-  function Counter() {
-    const count = useCounterStore(
-      store,
-      (state) => state.count,
-    );
-  
-    return (
-      <React.Fragment>
-        <button onClick={() => {
-          store.setState((prev) => ({ count: prev.count + 1 }));
-        }}>
-          inc
-        </button>
-        <button onClick={() => {
-          store.setState((prev) => ({ count: prev.count - 1 }));
-        }}>
-          dec
-        </button>
-        <h1>value: {count}</h1>
-      </React.Fragment>
-    )
-  }
-  
-  const { getByText, getByRole } = render(<Counter />);
-  fireEvent.click(getByText('inc'));
-  expect(getByRole('heading', { level: 1 }).textContent).toBe('value: 1');
+describe('외부 스토어', () => {
+  test('카운터', () => {
+    function Counter() {
+      const count = useCounterStore(store, (state) => state.count);
 
-  fireEvent.click(getByText('dec'));
-  expect(getByRole('heading', { level: 1 }).textContent).toBe('value: 0');
+      return (
+        <React.Fragment>
+          <button
+            onClick={() => {
+              store.setState((prev) => ({ count: prev.count + 1 }));
+            }}
+          >
+            inc
+          </button>
+          <button
+            onClick={() => {
+              store.setState((prev) => ({ count: prev.count - 1 }));
+            }}
+          >
+            dec
+          </button>
+          <h1>value: {count}</h1>
+        </React.Fragment>
+      );
+    }
+
+    const { getByText, getByRole } = render(<Counter />);
+    fireEvent.click(getByText('inc'));
+    expect(getByRole('heading', { level: 1 }).textContent).toBe('value: 1');
+
+    fireEvent.click(getByText('dec'));
+    expect(getByRole('heading', { level: 1 }).textContent).toBe('value: 0');
+  });
+
+  /** @todo 통과 시키자 */
+  test.skip('리렌더링', () => {
+    let renderCount = 0;
+    function Counter() {
+      const count = useCounterStore(store, (state) => state.count);
+      renderCount += 1;
+      return (
+        <React.Fragment>
+          <button
+            onClick={() => {
+              store.setState((prev) => ({ count: prev.count + 1 }));
+            }}
+          >
+            inc
+          </button>
+          <button
+            onClick={() => {
+              store.setState((prev) => ({ count: prev.count - 1 }));
+            }}
+          >
+            dec
+          </button>
+          <h1>value: {count}</h1>
+        </React.Fragment>
+      );
+    }
+
+    const { getByText } = render(<Counter />);
+    expect(renderCount).toBe(1);
+    fireEvent.click(getByText('dec'));
+    fireEvent.click(getByText('dec'));
+    fireEvent.click(getByText('dec'));
+    expect(renderCount).toBe(1);
+
+    fireEvent.click(getByText('inc'));
+    expect(renderCount).toBe(2);
+  });
 });
