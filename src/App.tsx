ea4658a846f3915ed9dd6b2d6845238d599df4b6
dkpark10 <dkpark10@naver.com>
2024-05-25 02:31:24 +0900

    feat: zustand3 코드 가져옴

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
 // import { useStore , createStore } from './core';
-import { useStore , createStore } from './core2';
+// import { useStore , createStore } from './core2';
+import { create } from './zustand3';
 
-const counterStore = createStore<{ count: number }>({ count: 0 });
+type Data = {
+  count: number;
+}
+
+type Setter = (arg0: (state: Data) => Data) => Data
+
+const useStore = create((set: Setter) => ({
+  count: 0,
+  setCount: (volume: number) => set((state: Data) => ({ count: state.count + volume })),
+}));
 
 export default function App() {
-  const [count, setState] = useStore(counterStore, (state) => state.count);
+  const { count, setCount } = useStore((state: Data) => state);
 
   return (
     <React.Fragment>
-      <button onClick={() => {
-        setState((prev) => ({ count: prev.count + 1 }));
-      }}>
+      <button
+        onClick={() => {
+          setCount(1);
+        }}
+      >
         increase
       </button>
-      <button onClick={() => {
-        setState((prev) => ({ count: prev.count }));
-      }}>
+      <button
+        onClick={() => {
+          setCount(-1);
+        }}
+      >
         decrease
       </button>
       <h1>value: {count}</h1>
     </React.Fragment>
-  )
+  );
 }
